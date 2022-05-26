24287d58c627fd5683d56b01b704ccd4b7d15c4c
Emile2020 <emiledebruyckere@gmail.com>
2022-05-26 15:37:24 +0200

    p3

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-const root = resolve(__dirname, 'src')
+const root = resolve(__dirname)
 const outDir = resolve(__dirname, 'dist')
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build : {
     rollupOptions: {
-      main: resolve(root, 'index.html'),
+      input:{
+        main: resolve(root, 'index.html'),
       aboutme: resolve(root, 'test.html'),
       home: resolve(root, 'home.html')
+      }
     }
   }
 })
